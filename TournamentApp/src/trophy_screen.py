d3cca3f6aea4a106294223f89fa9cabf2fa6c0b6
Jakubach <jakubach@gmail.com>
2025-01-10 18:51:18 +0100

    Revert "Added staged championship system"
    This reverts commit 73c742a4df08b4968861b1b033c40f81bd1154cd.

--- TournamentApp/src/trophy_screen.py
+++ TournamentApp/src/trophy_screen.py
@@ -3,65 +3,74 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.scrollview import ScrollView
-from teams_screen import RoundedButton
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.widget import Widget
-from trophy_stage_screen import TropyStageScreen
+
+class RankingTable(GridLayout):
+    def __init__(self, headers, data, **kwargs):
+        super().__init__(**kwargs)
+
+        self.size_hint_y = None
+        self.bind(minimum_height=self.setter('height'))
+        self.data = data
+        self.headers = headers
+        self.cols = len(self.headers)
+        self.sort_order = [True] * len(self.headers)
+
+        for i, header in enumerate(self.headers):
+            button = Button(
+                text=header,
+                size_hint_y=None,
+                height=50,
+                size_hint_x=1 / len(self.headers),
+            )
+            button.bind(on_release=lambda btn, col=i: self.sort_by_column(col))
+            self.add_widget(button)
+
+        self.sort_by_column(0)
+
+    def update_table(self):
+        self.clear_widgets()
+
+        for i, header in enumerate(self.headers):
+            button = Button(
+                text=header,
+                size_hint_y=None,
+                height=50,
+                size_hint_x=1 / len(self.headers),
+            )
+            button.bind(on_release=lambda btn, col = i : self.sort_by_column(col))
+            self.add_widget(button)
+
+        for row in self.data:
+            for cell in row:
+                self.add_widget(Label(
+                    text=cell,
+                    size_hint_y=None,
+                    height=40,
+                    size_hint_x=1 / len(self.headers),
+                    text_size=(100, None),
+                    halign='center',
+                    valign='middle'
+                ))
+
+    def sort_by_column(self, column_index):
+        self.sort_order[column_index] = not self.sort_order[column_index]
+        self.data.sort(key=lambda x: x[column_index], reverse = self.sort_order[column_index])
+        self.update_table()
+
 
 class TrophyScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.build()
 
-
-    def create_button(self, text, clicked_action = lambda text: print(text)):
-        button = RoundedButton(
-            text=text
-        )
-        button.bind(on_press=lambda x, button_text=text: clicked_action(button_text))
-        return button
-
     def build(self):
-        pass
-        # headers = ['Rank' , 'Team', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'Pts']
-        # data = [
-        #     [str(1), 'Team A', str(1), str(0), str(0), str(1), str(0), str(1),str(2)],
-        #     [str(2), 'Team B', str(0), str(1), str(0), str(1), str(1), str(0),str(1)],
-        #     [str(3), 'Team C', str(0), str(0), str(1), str(1), str(4), str(-3),str(0)],
-        # ]
-        # table = RankingTable(headers=headers, data=data)
-        # root = ScrollView(size_hint=(1, 1), bar_width=10)
-        # root.add_widget(table)
-        # self.add_widget(root)
-
-    def build(self):
-        self.clear_widgets()
-        layout = BoxLayout(
-            orientation="vertical"
-        )
-
-        stages = []
-        stages.append('Group stage')
-        stages.append('Final stage')
-
-        for stage in stages:
-            hor_layout = BoxLayout(
-            orientation="horizontal",
-            size_hint_y = 0.8 / len(stages)
-            )
-            hor_layout.add_widget(Widget(size_hint_x=0.1))
-
-            stage_btn = self.create_button(stage, self.stage_clicked)
-
-            hor_layout.add_widget(stage_btn)
-            hor_layout.add_widget(Widget(size_hint_x=0.1))
-            layout.add_widget(hor_layout)
-            layout.add_widget(Widget(
-                size_hint_y=0.2 / len(stages)
-            ))
-        self.add_widget(layout)
-
-    def stage_clicked(self, team_name: str):
-        obj = StageScreen(name='Stage')
-        self.parent.add_widget(obj)
-        self.parent.current = obj.name
+        headers = ['Rank' , 'Team', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'Pts']
+        data = [
+            [str(1), 'Team A', str(1), str(0), str(0), str(1), str(0), str(1),str(2)],
+            [str(2), 'Team B', str(0), str(1), str(0), str(1), str(1), str(0),str(1)],
+            [str(3), 'Team C', str(0), str(0), str(1), str(1), str(4), str(-3),str(0)],
+        ]
+        table = RankingTable(headers=headers, data=data)
+        root = ScrollView(size_hint=(1, 1), bar_width=10)
+        root.add_widget(table)
+        self.add_widget(root)
